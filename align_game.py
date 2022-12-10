c9cdde569b35daa062b1435a331932587c023674
Ozhcar <ozhcar@gmail.com>
2022-12-10 20:04:45 +0100

    Game setup + updated align checks

--- align_game.py
+++ align_game.py
@@ -2,6 +2,9 @@ from colorama import Back, Fore, Style
 from typing import List
 
 
+import re
+
+
 class FullColumnError(Exception):
     pass
 
@@ -66,7 +69,7 @@ class Grid:
     def check_column_validity(self, column: int) -> bool:
         return self.grid[0][column] == self.default_filler
 
-    def check_winner(self, align_count: int, player_symbol: str) -> bool:
+    def check_winner(self, align_count: int, player_symbol: str, skip_diags: bool) -> bool:
         # Columns check
         for i in range(self.columns):
             count = 0
@@ -99,6 +102,9 @@ class Grid:
                     count = 0
                     self.winning_tiles = []
 
+        if skip_diags:
+            return False
+
         # Left diagonals check
         for i in range(align_count - self.lines, self.columns - align_count + 1):
             count = 0
@@ -119,7 +125,7 @@ class Grid:
                     pass
 
         # Right diagonals check
-        for i in range(self.columns + self.lines - align_count, align_count - 1, -1):
+        for i in range(self.columns + self.lines - align_count - 1, align_count - 2, -1):
             count = 0
             self.winning_tiles = []
 
@@ -146,22 +152,18 @@ class Grid:
                 break
 
     def set_winning_tiles_background(self):
-
         for winning_tile in self.winning_tiles:
             tile_content = self.grid[winning_tile['line']][winning_tile['column']]
             self.grid[winning_tile['line']][winning_tile['column']] = Back.GREEN + tile_content
 
 
-class P4Game:
-    def __init__(self, grid: Grid, players: List[Player], align_count: int = 4):
+class AlignGame:
+    def __init__(self, grid: Grid, players: List[Player], align_count: int = 4, skip_diags: bool = False):
         self.grid = grid
         self.game_name = '4 in a Row'
-        self.align_count = align_count
-
-        if self.align_count > self.grid.columns and self.align_count > self.grid.lines:
-            raise UnwinnableGameError
-
         self.players = players
+        self.align_count = align_count
+        self.skip_diags = skip_diags
         self.turns = 0
         self.winner = None
         # self.token_colors = '🔴🟡🟠🔵🟢🟣🟤-⬤'
@@ -169,7 +171,7 @@ class P4Game:
     def __repr__(self):
         return '\n'.join([f'Player {self.players.index(player) + 1} : {player.nickname}' for player in self.players])
 
-    def get_current_player_data(self):
+    def get_current_player_data(self) -> dict:
         return {'number': (symbol := (self.turns - 1) % 2),
                 'symbol': f'\033[91m⬤{Style.RESET_ALL}' if symbol == 0 else f'\033[93m⬤{Style.RESET_ALL}'}
 
@@ -186,6 +188,7 @@ class P4Game:
 
             while True:
                 try:
+                    # Undo and restart functionalities
                     true_player_input = input('Which column you wanna play in ? ')
                     player_input = int(true_player_input) - 1
 
@@ -202,8 +205,8 @@ class P4Game:
 
             self.grid.set_token(player_data['symbol'], player_input)
 
-            if self.turns >= 2 * self.align_count - 1 and self.grid.check_winner(self.align_count,
-                                                                                 player_data['symbol']):
+            if self.turns >= 2 * self.align_count - 1 and \
+                    self.grid.check_winner(self.align_count, player_data['symbol'], self.skip_diags):
                 self.grid.set_winning_tiles_background()
                 self.winner = self.players[player_data['number']]
 
@@ -220,31 +223,144 @@ class P4Game:
             print(self.grid)
             print('One of you two is MAD lucky')
 
+        print()
 
-def main():
+
+def plural_adjuster(count: int, singular: str = '', plural: str = 's') -> str:
+    return plural if count >= 2 else singular
+
+
+def player_login(count: int = 2) -> List[Player]:
+    players = []
+
+    for i in range(count):
+        while True:
+            choice = input('Register or Login (r/l) ? ')
+
+            if choice.lower() in ['register', 'reg', 'r']:
+                nick = input(f'Player {i + 1} nickname ? ')
+                players.append(Player(nick))
+
+                break
+            elif choice.lower() in ['login', 'log', 'l']:
+                print('Soon')
+            else:
+                print('The action could not be recognized, try again')
+
+    return players
+
+
+def game_setup() -> dict:
     while True:
-        # Setup ; ask for the required values
+        lines = input('Number of lines and columns of the grid ? ')
+
+        match = re.compile(r'(\d+).+?(\d+)').search(lines)
+
+        if match:
+            grid_lines = int(match.groups()[0])
+            grid_columns = int(match.groups()[1])
+            break
+        else:
+            match = re.compile(r'(\d+)').search(lines)
+
+            if match:
+                if len(match.group()) == 1:
+                    grid_lines = int(match.group())
+                    grid_columns = int(match.group())
+                else:
+                    grid_lines = int(match.group()[:len(match.group()) // 2])
+                    grid_columns = int(match.group()[len(match.group()) // 2:])
+
+                break
+            else:
+                print('You must pass at least two integers to create a grid, try again')
+
+    while True:
+        skip_diags = False
+        align_count = 0
+        true_lines = ''
 
         try:
-            p4_game = P4Game(Grid(), [Player('Ozhcar'), Player('Biro')])
-            p4_game.play_game()
+            true_lines = input('Number of tokens to align on the grid ? ')
+            align_count = int(true_lines)
+
+            if align_count <= 0:
+                raise ValueError
+            if align_count > grid_columns and align_count > grid_lines:
+                raise UnwinnableGameError
+            elif align_count > grid_columns or align_count > grid_lines:
+                while True:
+                    choice = input(f'You won\'t be able to align {align_count} token{plural_adjuster(align_count)} '
+                                   f'in the diagonals, do you still want to proceed (y/n) ? ')
+
+                    if choice.lower() in ['yes', 'y']:
+                        skip_diags = True
+
+                        break
+                    elif choice.lower() in ['no', 'n']:
+                        break
+                    else:
+                        print('The action could not be recognized, try again')
+            else:
+                break
+
+            if skip_diags:
+                break
+        except ValueError:
+            print(f'You must pass a strictly positive integer which "{true_lines}" is not, try again')
         except UnwinnableGameError:
-            print(f'The number of tokens to align () exceeds the number of lines () and columns () of the game grid, '
-                  f'setup again')
+            print(f'The number of tokens to align ({align_count}) exceeds the number of lines ({grid_lines}) '
+                  f'and columns ({grid_columns}) of the game grid, try again')
+
+    return {'lines': grid_lines, 'columns': grid_columns, 'align_count': align_count, 'skip_diags': skip_diags}
+
+
+def setup_menu() -> dict:
+    while True:
+        choice = input('New Game or Resume Game (n/r) ? ')
+
+        if choice.lower() in ['new', 'n']:
+            return game_setup()
+        elif choice.lower() in ['resume', 'res', 'r']:
+            print('Soon')
+        else:
+            print('The action could not be recognized, try again')
+
+
+def main(align_game=None, players=None, data=None):
+    while True:
+        if not align_game:
+            players = player_login()
+
+        # if data ask if they wanna reshape the game
+        if not data:
+            data = setup_menu()  # Back functionnality
+
+        print(data['skip_diags'])
+
+        align_game = AlignGame(Grid(data['lines'], data['columns']), players, data['align_count'], data['skip_diags'])
+        align_game.play_game()
+
+        while True:
+            choice = input('Do you want to play again (y/n) ? ')
+
+            if choice.lower() in ['yes', 'y']:
+                break
+            elif choice.lower() in ['no', 'n']:
+                return
+            else:
+                print('The action could not be recognized, try again')
 
 
 main()
 
 
-# 1- If tokens to align > grid.lines and grid.columns : error, if only one of the two : warn and skip the diagonals
-# checks
-# 2 - Choosable nicknames
-# 3 - Choosable token color https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
-# 4 - Choosable default filler token color (like Fore.BLACK or Fore.WHITE)
-# 5 - Restart the game option
-# 6 - Undo turn if both players agree
-# 7 - Maybe save game option (resumable later)
-# 8 - Create, save and login to profiles
-# 9 - Update stats in json file
-# 10 - Use pygame for a better rendering
-# 11 - Optimize winner check : if on the kind of check you check there is still enough space for a potential alignment
+# 1 - Choosable token color https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+# 2 - Choosable default filler token color (like Fore.BLACK or Fore.WHITE)
+# 3 - Restart the game option
+# 4 - Undo turn if both players agree
+# 5 - Maybe save game option (resumable later)
+# 6 - Create, save and login to profiles
+# 7 - Update stats in json file
+# 8 - Use pygame for a better rendering
+# 9 - Optimize winner check : if on the kind of check you check there is still enough space for a potential alignment
